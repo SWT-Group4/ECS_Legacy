72b1b83ba5fb3e9798eda5d43cdc9e7a87cc6dff
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-09-21 00:47:16 +0200

    Add test files and fakes (tests are no good)

---
+++ ECS.Legacy.Test.Unit/FakeHeater.cs
@@ -0,0 +1,30 @@
+﻿namespace ECS.Legacy.Test.Unit
+{
+    public class FakeHeater : IHeater
+    {
+
+        // Test Variables:
+        public bool HeaterIsOn = false;
+        public bool HeaterIsOff = false;
+        // _TestVariables End
+
+        public void TurnOn()
+        {
+            // Substitute System.Console.WriteLine("Heater is on");
+            // With HeaterIsOn = true;
+            HeaterIsOn = true;
+        }
+
+        public void TurnOff()
+        {
+            // Substitute System.Console.WriteLine("Heater is off");
+            // With HeaterIsOff = true;
+            HeaterIsOff = true;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
